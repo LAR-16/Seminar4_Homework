22f69b1f62dacb5da3d2b1426cb829af634cd292
Alexandra <lebedeva1618@yandex.ru>
2022-12-09 17:28:34 +0300

    задача 27, сумма цифр числа

---
+++ task27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (n > 0)
+{
+    int a = n % 10;
+    count = count + a;
+    n /= 10;
+}
+Console.WriteLine($"Сумма цифр числа равна {count}");
