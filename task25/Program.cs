39fa56480dfbff4298c8e95fa4ec6b263ffe996a
Alexandra <lebedeva1618@yandex.ru>
2022-12-09 17:04:16 +0300

    задача 25, возведение числа в степень

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int product = 1;
+for(int i = 1; i <= z; i ++)
+    product = product * a;
+
+Console.WriteLine($"{a} в степени {z} равно {product}");
