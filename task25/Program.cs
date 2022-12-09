35e9feda5265076bdc36716cbab9671945883e02
Alexandra <lebedeva1618@yandex.ru>
2022-12-09 19:09:00 +0300

    задача 25, возведение числа в степень

--- task25/Program.cs
+++ task25/Program.cs
@@ -4,7 +4,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int z = Convert.ToInt32(Console.ReadLine());
 
-int product = 1;
+double product = 1;
 for(int i = 1; i <= z; i ++)
     product = product * a;
 
