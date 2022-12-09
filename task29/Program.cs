0b5364c5d5c9569867df5f2af96fb3d53b53d631
Alexandra <lebedeva1618@yandex.ru>
2022-12-09 17:45:51 +0300

    задача 29, ввод массива руками

---
+++ task29/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+for (int i = 0; i < n; i ++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        
+    }
+    Console.WriteLine();  
+Console.WriteLine($"[{string.Join(", ", array)}]");
